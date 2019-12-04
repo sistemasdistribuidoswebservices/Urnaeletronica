4f01a2c18e40a4e9f20662882ec71470f5509330
AlvaroPereiradoNascimento <alvaropereira15@gmail.com>
2019-12-04 01:43:56 -0200

    iniciado controller thymeleaf

---
+++ ProjetoUrna/src/main/java/si/projetourna/Usuário/entity/Controllers/CandidatoController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package si.projetourna.Usuário.entity.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Alvaro
+ */
+
+@Controller
+public class CandidatoController {
+    
+    
+}
